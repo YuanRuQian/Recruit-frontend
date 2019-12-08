d530b381cf1a966f8c2fd273bec8830f9d527508
YuanRuQian <yuanruqian@outlook.com>
2019-12-08 11:29:32 +0800

    删除了无用包 vue-progressbar 改用 vuetify data table 自带的 progress bar

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
     <v-app>
         <app-navigation></app-navigation>
         <v-content transition="slide-x-transition">
-            <vue-progress-bar></vue-progress-bar>
             <router-view></router-view>
         </v-content>
     </v-app>
@@ -10,13 +9,11 @@
 
 <script>
     import AppNavigation from '@/components/AppNavigation';
-    import VueProgressBar from 'vue-progressbar'
 
     export default {
         name: 'App',
         components: {
-            AppNavigation,
-            VueProgressBar
+            AppNavigation
         }
     };
 </script>
